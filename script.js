87b607fad0a84d55b9fcbac8b4f3b21d848cb9ce
accio-Akshaya <akshayaganesh0910@gmail.com>
2024-12-04 00:14:56 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
-let currentUrl = window.location.href;
-let urlLength = currentUrl.length;
-alert(`The length of the URL is: ${urlLength}`);
+	let currentUrl = window.location.href;
+    let urlLength = currentUrl.length;
+alert(`The length of the URL is:${urlLength}`)
